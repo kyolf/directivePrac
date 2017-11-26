9e32ca3dc8140dc90cdda36dbca7790c45106769
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-25 22:04:58 -0800

    got  the text todo.html to show now (not content

---
+++ prac1/prac1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let app1 = angular.module('app1', []);
+
+app1.controller('TodoController', function($scope){
+  // $scope.todos = [{name:'Drinking Oolong Tea', completed: true},
+  //                 {name:'Making Oolong Tea', completed: false},
+  //                 {name:'Help Someone', completed:true},
+  //                 {name:'Troll', completed:true}
+  //                ];
+  $scope.todos = {name:'Drinking Oolong Tea', completed: true};
+}); 
+
+app1.directive('todoList', function(){
+  return {
+    template: 'todo-list.html'
+  };
+});
