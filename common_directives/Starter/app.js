ba747c0fd85c0b69eff1df777f58d9ff43a5ed42
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 01:32:07 -0800

    initial commit for common directives lesson

---
+++ common_directives/Starter/app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    
+  $scope.handleUpper = function () {
+    return $filter('uppercase')($scope.handle);
+  };  
+  
+}]);
