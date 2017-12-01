8afeacd6b8f66bc98863a06d5910523ea9d033cd
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-30 20:28:27 -0800

    fixed app.js version filter

--- prac3/Starter/app.js
+++ prac3/Starter/app.js
@@ -1,7 +1,6 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
-  $scope.handle = '';
   $scope.handleUpper = function () {
     return $filter('uppercase')($scope.handle);
   };  
