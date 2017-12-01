f637ff30ee074f49f698df3a57c1e98828fa1717
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-30 20:26:26 -0800

    wrote uppercase filter versions in both app.js and index.html

--- prac3/Starter/app.js
+++ prac3/Starter/app.js
@@ -2,7 +2,8 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
   $scope.handle = '';
-  $scope.handleUpper = $filter('uppercase')($scope.handle);
-    
+  $scope.handleUpper = function () {
+    return $filter('uppercase')($scope.handle);
+  };  
     
 }]);
