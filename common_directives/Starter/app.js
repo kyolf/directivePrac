504cc0b84c733231e722de3c30800d7436350815
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 03:20:10 -0800

    Playing with ng-repeat along with ng-show inside ng-class div

--- common_directives/Starter/app.js
+++ common_directives/Starter/app.js
@@ -1,10 +1,16 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
-    
+  $scope.handle = '';  
   $scope.handleUpper = function () {
     return $filter('uppercase')($scope.handle);
   };  
 
   $scope.characters = 5;
+
+  $scope.rules = [
+    {rulename: 'Must be 5 characters'},
+    {rulename: 'Must not be used elsewhere'},
+    {rulename: 'Must be cool'}
+  ];
 }]);
